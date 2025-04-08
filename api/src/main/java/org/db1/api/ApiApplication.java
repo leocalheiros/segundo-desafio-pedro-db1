9b2f67a196db896cfbf21672baf00e716594cb6f
Pedro Bruce de Lima <pedrobrucelima@gmail.com>
2025-04-08 15:17:44 -0300

    :hammer: extraindo código de localidade para uma classe utils

--- api/src/main/java/org/db1/api/ApiApplication.java
+++ api/src/main/java/org/db1/api/ApiApplication.java
@@ -2,11 +2,9 @@ package org.db1.api;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.db1.api.domain.util.LocalDateTimeUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -15,7 +13,7 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-		log.info("Aplicação iniciou em {}", LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
+		log.info("Aplicação iniciou em {}", LocalDateTimeUtils.setSaoPauloDateTime());
 	}
 
 }
--- api/src/main/java/org/db1/api/domain/gamehistory/GameService.java
+++ api/src/main/java/org/db1/api/domain/gamehistory/GameService.java
@@ -6,13 +6,11 @@ import org.db1.api.domain.enums.MatchResult;
 import org.db1.api.domain.gamehistory.dto.GameDetailsDTO;
 import org.db1.api.domain.gamehistory.dto.GameResultDTO;
 import org.db1.api.domain.gamehistory.dto.GameStartDTO;
+import org.db1.api.domain.util.LocalDateTimeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Service
 public class GameService {
@@ -25,7 +23,7 @@ public class GameService {
     }
 
     public GameResultDTO play(GameStartDTO dto) {
-        log.info("Iniciando partida em {}", LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
+        log.info("Iniciando partida em {}", LocalDateTimeUtils.setSaoPauloDateTime());
         boolean hasPlayer1Won = dto.playerOneMove().beats(dto.playerTwoMove());
         boolean hasPlayer2Won = dto.playerTwoMove().beats(dto.playerOneMove());
 
@@ -51,7 +49,7 @@ public class GameService {
     }
 
     public Page<GameDetailsDTO> list(Pageable pageable) {
-        log.info("Listando as últimas partidas em {}", LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
+        log.info("Listando as últimas partidas em {}", LocalDateTimeUtils.setSaoPauloDateTime());
         return repository.findAllByOrderByPlayedInDesc(pageable).map(GameDetailsDTO::new);
     }
 }
